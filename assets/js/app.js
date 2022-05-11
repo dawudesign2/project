26800e8ff3f5b9a13875ae02bd139b7f408562f2
Dawudesign <contact@dawudesign.fr>
2022-05-11 16:46:34 +0200

    add modal and overlay

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+// Function Onclick ajouter la classe OPEN
+const onClick = (e) => {
+    e.preventDefault();
+    document.querySelector('#modal').classList.add('open');
+}
+// Function OnClose enleve la classe OPEN
+const onClose = (e) => {
+    e.preventDefault();
+    document.querySelector('#modal').classList.remove('open');
+}
+// Ecouteur
+document.querySelector('#button').addEventListener('click', onClick);
+document.querySelector('.close').addEventListener('click', onClose);
