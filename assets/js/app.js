5a64219d8733ece2869d8795f5d830ea1698194a
Dawudesign <contact@dawudesign.fr>
2022-05-12 16:45:21 +0200

    add app

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,25 @@
-// Function Onclick ajouter la classe OPEN
-const onClick = (e) => {
+/*(function () {
+    // Function Onclick ajouter la classe OPEN
+    const onClick = (e) => {
+        e.preventDefault();
+        document.querySelector('#modal').classList.add('open');
+    }
+    // Function OnClose enleve la classe OPEN
+    const onClose = (e) => {
+        e.preventDefault();
+        document.querySelector('#modal').classList.remove('open');
+    }
+    // Ecouteur
+    document.querySelector('#button').addEventListener('click', onClick);
+    document.querySelector('.close').addEventListener('click', onClose);
+})
+*/
+const onNav = (e) => {
     e.preventDefault();
-    document.querySelector('#modal').classList.add('open');
+    const open = document.querySelector('.openNav');
+    document.querySelector('nav').classList.add('openNav');
+    if(open){
+        document.querySelector('nav').classList.remove('openNav');
+    }
 }
-// Function OnClose enleve la classe OPEN
-const onClose = (e) => {
-    e.preventDefault();
-    document.querySelector('#modal').classList.remove('open');
-}
-// Ecouteur
-document.querySelector('#button').addEventListener('click', onClick);
-document.querySelector('.close').addEventListener('click', onClose);
-
+document.querySelector('#headerIcon').addEventListener('click', onNav);
