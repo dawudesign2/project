c68f01e34996f9bf0e1b6ede88b380ef52025ddd
Dawudesign <contact@dawudesign.fr>
2022-05-15 10:10:54 +0200

    add carousel

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+//Modal
 (function () {
     // Function Onclick ajouter la classe OPEN
     const onClick = (e) => {
@@ -13,8 +14,9 @@
     document.querySelector('#btn').addEventListener('click', onClick);
     document.querySelector('.close').addEventListener('click', onClose);
 })();
-
+//NavBar
 (function () {  
+     // Function Onclick ajouter la classe OPENNav
     const onNav = (e) => {
         e.preventDefault();
         const open = document.querySelector('.openNav');
@@ -24,4 +26,50 @@
         }
     }
     document.querySelector('#headerIcon').addEventListener('click', onNav);
+})();
+//Explore 
+(function() {
+     // Function Onclick ajouter la classe OnExolorer
+    const onExplore = (e) => {
+        e.preventDefault();
+        document.querySelector('.explore').classList.add('onExplore');
+    }
+    const offExplore = (e) => {
+        e.preventDefault();
+        document.querySelector('.explore').classList.remove('onExplore');
+
+    }
+    document.querySelector('#explore').addEventListener('click', onExplore);
+    document.querySelector('#close').addEventListener('click', offExplore);
+})();
+
+//Carousel 
+(function(){
+    const delay = 3000; //ms
+    const slides = document.querySelector('.slides');
+    const count = slides.childElementCount;
+    const maxLeft = (count - 1 ) * 100 * -1;
+
+    let current = 0;
+    const changeSlide = (next = true) => {
+        if(next){
+            current += current > maxLeft ? - 100 : current * -1;
+        }else {
+            current = current < 0 ? current + 100 : maxLeft;
+        }
+        slides.style.left = current + "%";
+    }
+    let auto = setInterval(changeSlide, delay);
+    const restart = () => {
+        clearInterval(auto);
+        auto = setInterval(changeSlide, delay);
+    }
+    document.querySelector(".nextSlide").addEventListener('click', () => {
+        changeSlide();
+        restart();
+    });
+    document.querySelector(".prevSlide").addEventListener('click', () => {
+        changeSlide(false);
+        restart();
+    })
 })();
